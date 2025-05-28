ec0ebf57ab54622948e8ffc9edd64e54e7571b03
dutreras <dutreras369@gmail.com>
2025-05-27 21:35:05 -0400

    add landing modular

---
+++ page-landing-modular.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Template Name: Landing Modular Unificada
+ */
+get_header();
+
+$titulo = get_field('titulo_landing');
+$subtitulo = get_field('subtitulo_landing');
+$imagen_hero = get_field('imagen_hero');
+?>
+
+<section class="landing-hero" style="background-image: url('<?php echo esc_url($imagen_hero['url']); ?>');">
+  <div class="container" data-aos="fade-up">
+    <h1><?php echo esc_html($titulo); ?></h1>
+    <p><?php echo esc_html($subtitulo); ?></p>
+  </div>
+</section>
+
+<section class="landing-content" data-aos="fade-up">
+  <div class="container">
+    <?php
+      while (have_posts()) : the_post();
+        the_content(); // Aquí insertas los shortcodes [ev-cursos], [ev-terapias], etc.
+      endwhile;
+    ?>
+  </div>
+</section>
+
+<?php get_footer(); ?>
