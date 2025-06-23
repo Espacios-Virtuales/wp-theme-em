dbbf19b5dc62377f19e7f8f5807729b807c4a2d9
dutreras <dutreras369@gmail.com>
2025-06-23 16:21:49 -0400

    refactor structure

--- inc/init.php
+++ inc/init.php
@@ -11,4 +11,4 @@ require_once __DIR__ . '/shorcodes/init-shorcodes.php';
 require_once __DIR__ . '/modules/init-modules.php';
 
 // ✨ Helpers 
-require_once __DIR__ . '/modules/init-helpers.php';
+require_once __DIR__ . '/helpers/init-helpers.php';
