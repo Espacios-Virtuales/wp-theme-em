ba62cf45f71d9c3e17d45455279753e539e64d7a
dutreras <dutreras369@gmail.com>
2025-08-29 18:17:18 -0400

    update shorcode seo

--- inc/shortcodes/ev-seo-intro.php
+++ inc/shortcodes/ev-seo-intro.php
@@ -75,9 +75,9 @@ function ev_seo_intro_shortcode($atts = []) {
           <div class="seo-intro__content mx-auto"><?php echo $body_html; ?></div>
 
           <?php if (!empty($links)) : ?>
-            <ul class="seo-intro__links">
+            <ul class="seo-intro__links list-unstyled">
               <?php foreach ($links as $l) : ?>
-                <li class="mb-1">→ <a href="<?php echo esc_url($l['url']); ?>"><?php echo esc_html($l['label']); ?></a></li>
+                <li><a href="<?php echo esc_url($l['url']); ?>"><?php echo esc_html($l['label']); ?></a></li>
               <?php endforeach; ?>
             </ul>
           <?php endif; ?>
