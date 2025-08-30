338382e2fab90e7454445f60fb14d649e53ba860
dutreras <dutreras369@gmail.com>
2025-08-29 20:31:39 -0400

    repair blog post 1.1

--- comments.php
+++ comments.php
@@ -1,76 +1,52 @@
 <?php
-
 /**
- * The template for displaying comments
- *
- * This is the template that displays the area of the page that contains both the current comments
- * and the comment form.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package blog-theme
+ * Template for displaying comments and comment form
+ * @package escuela-mistica
  */
 
-/*
- * If the current post is protected by a password and
- * the visitor has not yet entered the password we will
- * return early without loading the comments.
- */
-if (post_password_required()) {
-	return;
-}
+if (post_password_required()) return;
 ?>
 
-
 <div id="comments" class="comments-area container-fluid">
-	<div class="row">
-		<div class="col-lg-10 col-xl-8">
+  <div class="row justify-content-center">
+    <div class="col-lg-10 col-xl-8">
 
+      <?php if (have_comments()) : ?>
+        <h2 class="comments-title">
+          <?php
+          $comment_count = get_comments_number();
+          printf(
+            esc_html(_n(
+              'Un comentario en “%2$s”',
+              '%1$s comentarios en “%2$s”',
+              $comment_count,
+              'escuelamistica'
+            )),
+            number_format_i18n($comment_count),
+            get_the_title()
+          );
+          ?>
+        </h2>
 
-			<?php if (have_comments()) : ?>
-				<h2 class="comments-title">
-					<?php
-					$comment_count = get_comments_number();
-					if ('1' === $comment_count) {
-						printf(
-							esc_html__('One comment on &ldquo;%s&rdquo;', 'tiendavirtual'),
-							get_the_title()
-						);
-					} else {
-						printf(
-							esc_html(_nx('%1$s comment on &ldquo;%2$s&rdquo;', '%1$s comments on &ldquo;%2$s&rdquo;', $comment_count, 'comments title', 'tiendavirtual')),
-							number_format_i18n($comment_count),
-							get_the_title()
-						);
-					}
-					?>
-				</h2>
+        <ul class="comment-list">
+          <?php
+          wp_list_comments([
+            'style'      => 'ul',
+            'short_ping' => true,
+            'avatar_size' => 60,
+          ]);
+          ?>
+        </ul>
 
-				<ul class="comment-list">
-					<?php
-					wp_list_comments(
-						array(
-							'style'      => 'ul',
-							'short_ping' => true,
-							'avatar_size' => 60,
-						)
-					);
-					?>
-				</ul>
+        <?php the_comments_navigation(); ?>
 
-				<?php
-				the_comments_navigation();
+        <?php if (!comments_open()) : ?>
+          <p class="no-comments"><?php esc_html_e('Los comentarios están cerrados.', 'escuelamistica'); ?></p>
+        <?php endif; ?>
+      <?php endif; ?>
 
-				if (! comments_open()) :
-				?>
-					<p class="no-comments"><?php esc_html_e('Comments are closed.', 'tiendavirtual'); ?></p>
-			<?php
-				endif;
+      <?php comment_form(); ?>
 
-			endif; // Check for have_comments().
-
-			comment_form();
-			?>
-		</div>
-	</div>
-</div><!-- #comments -->
+    </div>
+  </div>
+</div>
