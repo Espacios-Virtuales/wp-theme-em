41248793d813d02b9338c13c3e555ab30001ba8d
dutreras <dutreras369@gmail.com>
2025-08-29 20:10:38 -0400

    repair blog post 1.1

--- comments.php
+++ comments.php
@@ -23,7 +23,7 @@ if (post_password_required()) {
 
 
 <div id="comments" class="comments-area container-fluid">
-	<div class="row justify-content-center">
+	<div class="row">
 		<div class="col-lg-10 col-xl-8">
 
 
