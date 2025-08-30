6f8f1afd157277819501abe74ee8ad0bdf144f56
dutreras <dutreras369@gmail.com>
2025-08-29 20:09:26 -0400

    repair blog post 1.1

--- comments.php
+++ comments.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying comments
  *
@@ -15,57 +16,61 @@
  * the visitor has not yet entered the password we will
  * return early without loading the comments.
  */
-if ( post_password_required() ) {
+if (post_password_required()) {
 	return;
 }
 ?>
 
 
-<div id="comments container-fluid" class="comments-area">
+<div id="comments" class="comments-area container-fluid">
+	<div class="row justify-content-center">
+		<div class="col-lg-10 col-xl-8">
 
-	<?php if (have_comments()) : ?>
-		<h2 class="comments-title">
+
+			<?php if (have_comments()) : ?>
+				<h2 class="comments-title">
+					<?php
+					$comment_count = get_comments_number();
+					if ('1' === $comment_count) {
+						printf(
+							esc_html__('One comment on &ldquo;%s&rdquo;', 'tiendavirtual'),
+							get_the_title()
+						);
+					} else {
+						printf(
+							esc_html(_nx('%1$s comment on &ldquo;%2$s&rdquo;', '%1$s comments on &ldquo;%2$s&rdquo;', $comment_count, 'comments title', 'tiendavirtual')),
+							number_format_i18n($comment_count),
+							get_the_title()
+						);
+					}
+					?>
+				</h2>
+
+				<ul class="comment-list">
+					<?php
+					wp_list_comments(
+						array(
+							'style'      => 'ul',
+							'short_ping' => true,
+							'avatar_size' => 60,
+						)
+					);
+					?>
+				</ul>
+
+				<?php
+				the_comments_navigation();
+
+				if (! comments_open()) :
+				?>
+					<p class="no-comments"><?php esc_html_e('Comments are closed.', 'tiendavirtual'); ?></p>
 			<?php
-			$comment_count = get_comments_number();
-			if ('1' === $comment_count) {
-				printf(
-					esc_html__('One comment on &ldquo;%s&rdquo;', 'tiendavirtual'),
-					get_the_title()
-				);
-			} else {
-				printf(
-					esc_html(_nx('%1$s comment on &ldquo;%2$s&rdquo;', '%1$s comments on &ldquo;%2$s&rdquo;', $comment_count, 'comments title', 'tiendavirtual')),
-					number_format_i18n($comment_count),
-					get_the_title()
-				);
-			}
+				endif;
+
+			endif; // Check for have_comments().
+
+			comment_form();
 			?>
-		</h2>
-
-		<ul class="comment-list">
-			<?php
-			wp_list_comments(
-				array(
-					'style'      => 'ul',
-					'short_ping' => true,
-					'avatar_size' => 60,
-				)
-			);
-			?>
-		</ul>
-
-		<?php
-		the_comments_navigation();
-
-		if (! comments_open()) :
-		?>
-			<p class="no-comments"><?php esc_html_e('Comments are closed.', 'tiendavirtual'); ?></p>
-	<?php
-		endif;
-
-	endif; // Check for have_comments().
-
-	comment_form();
-	?>
-
+		</div>
+	</div>
 </div><!-- #comments -->
