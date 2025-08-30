c9b9c25d561192d4cd3239bcc10acddbbfb8b0ec
dutreras <dutreras369@gmail.com>
2025-08-29 21:46:28 -0400

    update shortcode seo

--- index.php
+++ index.php
@@ -43,7 +43,7 @@ get_header();
                                     <article id="post-<?php the_ID(); ?>" <?php post_class('card shadow-sm'); ?>>
                                         <?php if (has_post_thumbnail()) : ?>
                                             <div class="card-img-top d-flex justify-content-center">
-                                                <?php the_post_thumbnail('small', array('class' => 'img-fluid img-round')); ?>
+                                                <?php the_post_thumbnail('medium', ['class' => 'img-round']); ?>
                                             </div>
 
                                         <?php endif; ?>
