07297b7b9bc5fd88335d26fe7511ee2b071d860f
dutreras <dutreras369@gmail.com>
2025-10-26 12:27:24 -0300

    update style

--- header.php
+++ header.php
@@ -87,10 +87,3 @@
         </div>
       </nav>
     </header>
-
-    <?php
-    // Opcional: mover esto a front-page.php para mejor separación.
-    if (is_front_page()) {
-      echo do_shortcode('[ev-hero]');
-    }
-    ?>
--- page.php
+++ page.php
@@ -12,25 +12,29 @@
  * @package blog-theme
  */
 
-get_header();
-
-?>
-
-<main id="primary" class="site-main">
-  <div class="container-fluid">
-    <?php
-    while ( have_posts() ) :
-      the_post();
-
-      get_template_part( 'template-parts/content', 'page' );
-
-      if ( comments_open() || get_comments_number() ) :
-        comments_template();
-      endif;
-
-    endwhile;
-    ?>
-  </div>
-</main>
-
-<?php get_footer();
+ get_header();
+ ?>
+ 
+   <main id="primary" class="site-main">
+     <div class="container pt-5">
+       <?php
+       while ( have_posts() ) :
+         the_post();
+ 
+         get_template_part( 'template-parts/content', 'page' );
+ 
+         // If comments are open or we have at least one comment, load up the comment template.
+         if ( comments_open() || get_comments_number() ) :
+           comments_template();
+         endif;
+ 
+       endwhile; // End of the loop.
+       ?>
+ 
+     </div>
+ 
+   </main><!-- #main -->
+ 
+ <?php
+ get_footer();
+ 
