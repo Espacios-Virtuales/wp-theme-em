de17fdffd6221f4980417a650af073821352cebb
dutreras <dutreras369@gmail.com>
2025-11-06 19:20:08 -0300

    actualizar seo

--- inc/helpers/seo-prepend.php
+++ inc/helpers/seo-prepend.php
@@ -8,12 +8,12 @@ add_filter('the_content', function($content) {
     return $content;
   }
 
-  // Si el campo ACF 'seo_enabled' está activo y no se insertó manualmente
+  /* Si el campo ACF 'seo_enabled' está activo y no se insertó manualmente
   if (get_field('seo_enabled') && strpos($content, '[ev-seo_intro]') === false) {
     $intro = do_shortcode('[ev-seo_intro]');
     // Lo agregamos al final del contenido
     return $content . "\n\n" . $intro;
-  }
+  } */
 
   return $content;
 }, 999); // prioridad alta → se ejecuta al final
